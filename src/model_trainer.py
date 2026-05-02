ede3bd7c291c34b4eb4cc7642dc010f29a94ea9d
u201623659 <benghy.lp@gmail.com>
2026-05-02 01:45:00 -0500

    Script Model trainer

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -3,7 +3,8 @@ from sklearn.metrics import accuracy_score
 
 def train_model(X_train, y_train, X_test, y_test):
     clf = RandomForestClassifier(n_estimators=50)
+    model_name = 'Random_Forest'
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
-    return clf, accuracy
+    return clf, model_name,accuracy
